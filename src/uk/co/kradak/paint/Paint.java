789ce1a6433bcb36eecd20df4f8ae31ab7f72916
Callum Armitage <callum.thepro@hotmail.co.uk>
2013-05-29 22:01:11 +0100

    Paint Idea Created
    Paint idea was created and development was started

---
+++ src/uk/co/kradak/paint/Paint.java
@@ -0,0 +1,14 @@
+package uk.co.kradak.paint;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Paint extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
